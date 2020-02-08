20999eb3a6d33de14534a4953f33a18eef478118
Naoki Miyoshi <trombone344@gmail.com>
2020-02-08 19:29:24 +0900

    fix: 画面の基本ページを追加

--- dashboard/school-recomender/src/components/HelloWorld.vue
+++ dashboard/school-recomender/src/components/HelloWorld.vue
@@ -1,151 +1,100 @@
 <template>
   <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
+    <v-item-group
+      :mandatory="mandatory"
+      :multiple="multiple"
+    >
+      <v-container class="pa-0">
+        <v-row>
+			<v-item class="col-md-4 col-12">
+              <v-card
+                v-if="type === 'cards'"
+                :color="active ? 'light-green lighten-4' : ''"
+                class="d-flex align-center"
+                white
+                height="200"
+                @click="toggle"
+              >
+                <v-row justify="center">
+                  <v-dialog v-model="dialog" persistent max-width="290">
+                    <template v-slot:activator="{ on }">
+                      <v-btn color="primary" dark v-on="on">Open Dialog</v-btn>
+                    </template>
+                    <v-card>
+                      <v-card-title class="headline">Use Google's location service?</v-card-title>
+                      <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="green darken-1" text @click="dialog = false">Disagree</v-btn>
+                        <v-btn color="green darken-1" text @click="dialog = false">Agree</v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-dialog>
+                </v-row>
+              </v-card>
+			</v-item>
+          <v-col
+            v-for="n in 3"
+            :key="n"
+            cols="12"
+            md="4"
           >
-            {{ next.text }}
-          </a>
+            <v-item v-slot:default="{ active, toggle }">
+              <v-card
+                v-if="type === 'cards'"
+                :color="active ? 'light-green lighten-4' : ''"
+                class="d-flex align-center"
+                white
+                height="200"
+                @click="toggle"
+              >
+                <v-card-text>
+                  <div>Word of the Day</div>
+                  <p class="display-1 text--primary">
+                    be•nev•o•lent
+                  </p>
+                  <p>adjective</p>
+                  <div class="text--primary">
+                    well meaning and kindly.<br>
+                    "a benevolent smile"
+                  </div>
+                </v-card-text>
+                <v-scroll-y-transition>
+                  <div
+                    v-if="active"
+                    class="display-3 flex-grow-1 text-center font-weight-bold"
+					color="light-green darken-4"
+                  >
+                    Add
+                  </div>
+                </v-scroll-y-transition>
+              </v-card>
+            </v-item>
+          </v-col>
         </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
+      </v-container>
+    </v-item-group>
   </v-container>
 </template>
 
 <script>
+  import { VCard, VCardText } from 'vuetify/lib'
   export default {
     name: 'HelloWorld',
-
+	components: {
+      VCard,
+      VCardText,
+      // VCardActions,
+    },
     data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
+      types: [
+        'cards',
+        'images',
       ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/layout/pre-defined',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
+      type: 'cards',
+      mandatory: false,
+      multiple: true,
+      dialog: false,
     }),
   }
 </script>
