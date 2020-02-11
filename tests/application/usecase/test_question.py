645cc9b7094aea41becec0bbde193825da04eb7b
Naoki Miyoshi <trombone344@gmail.com>
2020-02-11 22:14:17 +0900

    fix: テストの修正

--- tests/application/usecase/test_question.py
+++ tests/application/usecase/test_question.py
@@ -1,3 +1,4 @@
+import freezegun
 from copy import deepcopy
 from unittest import TestCase
 from app.model.question.question import Question
@@ -9,25 +10,39 @@ from unittest.mock import MagicMock
 
 
 class CreateQuestionTest(TestCase):
-    def test_run(self):
-        question_dict = {
-            'register_user_id': 30,
-            'title': 'Test Question Title',
-            'contents': 'Test Question Contents',
-            # TODO: 日付フォーマットは後でかくにん
-            'register_date': '2019-02-20 09:01:00.000+09:00',
+    def setUp(self):
+        self.question_dict = {
+            'question_id': 1,
+            'register_user_id': "fjeiwo0g-rfar-fae",
+            'estimated_time': 15,
+            'question_sentence': {
+                'text': 'Question1 XXXX is ???',
+                'image_url': 'https://xxxxxxxxxxxxxxx.jpg'
+            },
+            'question_answer': {
+                'text': 'Question1 XXXX is ???',
+                'image_url': 'https://xxxxxxxxxxxxxxx.jpg'
+            },
+            'question_commentary': {
+                'text': 'Question1 XXXX is ???',
+                'image_url': 'https://xxxxxxxxxxxxxxx.jpg'
+            },
+            'register_date': '2020-02-11T20:20:18.033712+09:00',
             'subject_type': 'math',
-            'caption': None
+            'sort_tag_list': ['数学I', '初級']
             }
+
+    @freezegun.freeze_time('2020-02-11T20:20:18.033712+09:00')
+    def test_run(self):
         datasource = MagicMock()
         datasource.fetch_sequesnse_id = MagicMock(return_value=1)
         usecase = CreateQuestion(
             question_datasource=datasource, logger=MagicMock())
-        result = usecase.run(deepcopy(question_dict))
+        result = usecase.run(deepcopy(self.question_dict))
 
         expect = {
             'question_id': 1,
-            **question_dict
+            **self.question_dict
         }
         datasource.fetch_sequesnse_id.assert_called_once()
         datasource.insert_item.assert_called_once_with(
@@ -37,60 +52,76 @@ class CreateQuestionTest(TestCase):
 
 
 class UpdateQuestionTest(TestCase):
-    def test_run_ok(self):
-        question = {
+    def setUp(self):
+        self.question_dict = {
             'question_id': 1,
-            'register_user_id': 30,
-            'title': 'Test Question Title',
-            'contents': 'Test Question Contents',
-            # TODO: 日付フォーマットは後でかくにん
-            'register_date': '2019-02-20 09:01:00.000+09:00', 'subject_type': 'math',
-            'caption': None
+            'register_user_id': "fjeiwo0g-rfar-fae",
+            'estimated_time': 15,
+            'question_sentence': {
+                'text': 'Question1 XXXX is ???',
+                'image_url': 'https://xxxxxxxxxxxxxxx.jpg'
+            },
+            'question_answer': {
+                'text': 'Question1 XXXX is ???',
+                'image_url': 'https://xxxxxxxxxxxxxxx.jpg'
+            },
+            'question_commentary': {
+                'text': 'Question1 XXXX is ???',
+                'image_url': 'https://xxxxxxxxxxxxxxx.jpg'
+            },
+            'register_date': '2020-02-11T20:20:18.033712+09:00',
+            'subject_type': 'math',
+            'sort_tag_list': ['数学I', '初級']
             }
+
+    def test_run_ok(self):
         datasource = MagicMock()
         usecase = UpdateQuestion(
             question_datasource=datasource, logger=MagicMock())
-        result = usecase.run(QuestionId(1), deepcopy(question))
+        result = usecase.run(QuestionId(1), deepcopy(self.question_dict))
 
         datasource.fetch_sequesnse_id.assert_not_called()
         datasource.put_item.assert_called_once_with(
-            Question.from_dict(question))
-        self.assertEqual(result.to_dict(), question)
+            Question.from_dict(self.question_dict))
+        self.assertEqual(result.to_dict(), self.question_dict)
 
     def test_run_mismutch_question_id(self):
-        question = {
-            'question_id': 1,
-            'register_user_id': 30,
-            'title': 'Test Question Title',
-            'contents': 'Test Question Contents',
-            # TODO: 日付フォーマットは後でかくにん
-            'register_date': '2019-02-20 09:01:00.000+09:00', 'subject_type': 'math',
-            'caption': None
-            }
         datasource = MagicMock()
         usecase = UpdateQuestion(
             question_datasource=datasource, logger=MagicMock())
         with self.assertRaises(Exception):
-            usecase.run(QuestionId(2), deepcopy(question))
+            usecase.run(QuestionId(2), deepcopy(self.question))
 
 class FindQuestionTest(TestCase):
-    def test_run_ok(self):
-        question = {
+    def setUp(self):
+        self.question_dict = {
             'question_id': 1,
-            'register_user_id': 30,
-            'title': 'Test Question Title',
-            'contents': 'Test Question Contents',
-            # TODO: 日付フォーマットは後でかくにん
-            'register_date': '2019-02-20 09:01:00.000+09:00',
+            'register_user_id': "fjeiwo0g-rfar-fae",
+            'estimated_time': 15,
+            'question_sentence': {
+                'text': 'Question1 XXXX is ???',
+                'image_url': 'https://xxxxxxxxxxxxxxx.jpg'
+            },
+            'question_answer': {
+                'text': 'Question1 XXXX is ???',
+                'image_url': 'https://xxxxxxxxxxxxxxx.jpg'
+            },
+            'question_commentary': {
+                'text': 'Question1 XXXX is ???',
+                'image_url': 'https://xxxxxxxxxxxxxxx.jpg'
+            },
+            'register_date': '2020-02-11T20:20:18.033712+09:00',
             'subject_type': 'math',
-            'caption': None
+            'sort_tag_list': ['数学I', '初級']
             }
+
+    def test_run_ok(self):
         datasource = MagicMock()
         datasource.find_by_id = MagicMock(
-            return_value=Question.from_dict(question))
+            return_value=Question.from_dict(self.question_dict))
         usecase = FindQuestion(
             question_datasource=datasource, logger=MagicMock())
         result = usecase.run(QuestionId(1))
 
         datasource.find_by_id.assert_called_once_with(QuestionId(1))
-        self.assertEqual(result.to_dict(), question)
+        self.assertEqual(result.to_dict(), self.question_dict)
--- tests/model/question/test_question.py
+++ tests/model/question/test_question.py
@@ -1,34 +1,87 @@
+import freezegun
 from unittest import TestCase
 from app.model.question.question import Question
+from datetime import datetime
 
 
 class QuestionTest(TestCase):
-    def test_dict_min(self):
+    def test_dict_max(self):
         question_dict = {
             'question_id': 1,
-            'register_user_id': 30,
-            'title': 'Test Question Title',
-            'contents': 'Test Question Contents',
-            # TODO: 日付フォーマットは後でかくにん
-            'register_date': '2019-02-20 09:01:00.000+09:00',
+            'register_user_id': "fjeiwo0g-rfar-fae",
+            'estimated_time': 15,
+            'question_sentence': {
+                'text': 'Question1 XXXX is ???',
+                'image_url': 'https://xxxxxxxxxxxxxxx.jpg'
+            },
+            'question_answer': {
+                'text': 'Question1 XXXX is ???',
+                'image_url': 'https://xxxxxxxxxxxxxxx.jpg'
+            },
+            'question_commentary': {
+                'text': 'Question1 XXXX is ???',
+                'image_url': 'https://xxxxxxxxxxxxxxx.jpg'
+            },
+            'register_date': '2020-02-11T20:20:18.033712+09:00',
             'subject_type': 'math',
-            'caption': None
+            'sort_tag_list': ['数学I', '初級']
             }
 
         self.assertEqual(
             question_dict,
             Question.from_dict(question_dict).to_dict())
 
-    def test_dict_max(self):
+
+    @freezegun.freeze_time('2020-02-11T20:20:18.033712+09:00')
+    def test_create(self):
+        question_dict = {
+            'register_user_id': "fjeiwo0g-rfar-fae",
+            'estimated_time': 15,
+            'question_sentence': {
+                'text': 'Question1 XXXX is ???',
+                'image_url': 'https://xxxxxxxxxxxxxxx.jpg'
+            },
+            'question_answer': {
+                'text': 'Question1 XXXX is ???',
+                'image_url': 'https://xxxxxxxxxxxxxxx.jpg'
+            },
+            'question_commentary': {
+                'text': 'Question1 XXXX is ???',
+                'image_url': 'https://xxxxxxxxxxxxxxx.jpg'
+            },
+            'subject_type': 'math',
+            'sort_tag_list': ['数学I', '初級']
+            }
+        expect = {
+            **question_dict,
+            'question_id': 2,
+            'register_date': '2020-02-11T20:20:18.033712+09:00',
+        }
+        print(Question.create(2, question_dict).to_dict())
+        self.assertEqual(
+            expect,
+            Question.create(2, question_dict).to_dict())
+
+    def test_dict_min(self):
         question_dict = {
             'question_id': 1,
-            'register_user_id': 30,
-            'title': 'Test Question Title',
-            'contents': 'Test Question Contents',
-            # TODO: 日付フォーマットは後でかくにん
-            'register_date': '2019-02-20 09:01:00.000+09:00',
+            'register_user_id': "fjeiwo0g-rfar-fae",
+            'estimated_time': 15,
+            'question_sentence': {
+                'text': 'Question1 XXXX is ???',
+                'image_url': None
+            },
+            'question_answer': {
+                'text': 'Question1 XXXX is ???',
+                'image_url': None
+            },
+            'question_commentary': {
+                'text': 'Question1 XXXX is ???',
+                'image_url': None
+            },
+            'register_date': '2020-02-11T20:20:18.033712+09:00',
             'subject_type': 'math',
-            'caption': 'caption'
+            'sort_tag_list': ['数学I', '初級']
             }
 
         self.assertEqual(
