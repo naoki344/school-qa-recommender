059490134525e2646eb88e2ad944e1800317eaa0
濱砂大輝 <vetmal.0377.d@gmail.com>
2020-02-25 21:44:51 +0900

    fix: data内の変数名を再定義しました。

--- dashboard/school-recomender/src/components/user/SignUp.vue
+++ dashboard/school-recomender/src/components/user/SignUp.vue
@@ -10,7 +10,7 @@
     <v-card-text>
       <v-form>
         <v-text-field
-          v-model="username"
+          v-model="email"
           prepend-icon="mdi-email"
           counter="25"
           hint="メールアドレスを入力"
@@ -18,7 +18,7 @@
           required
         ></v-text-field>
         <v-text-field
-          v-model="username"
+          v-model="emailConfirm"
           prepend-icon="mdi-email"
           counter="25"
           label="Email(確認)"
@@ -27,18 +27,18 @@
         <v-text-field v-model="nickname" prepend-icon counter="10" label="ニックネーム(表示名)" required></v-text-field>
         <v-row>
           <v-col>
-            <v-text-field v-model="username" prepend-icon counter="10" label="姓" required></v-text-field>
+            <v-text-field v-model="lastName" prepend-icon counter="10" label="姓" required></v-text-field>
           </v-col>
           <v-col>
-            <v-text-field v-model="username" prepend-icon counter="10" label="名" required></v-text-field>
+            <v-text-field v-model="firstName" prepend-icon counter="10" label="名" required></v-text-field>
           </v-col>
         </v-row>
         <v-row>
           <v-col>
-            <v-text-field v-model="username_kana" prepend-icon counter="10" label="姓(カナ)" required></v-text-field>
+            <v-text-field v-model="lastNameKana" prepend-icon counter="10" label="姓(カナ)" required></v-text-field>
           </v-col>
           <v-col>
-            <v-text-field v-model="username_kana" prepend-icon counter="10" label="名(カナ)" required></v-text-field>
+            <v-text-field v-model="firstNameKana" prepend-icon counter="10" label="名(カナ)" required></v-text-field>
           </v-col>
         </v-row>
         <v-text-field
@@ -53,14 +53,14 @@
           required
         ></v-text-field>
         <v-text-field
-          v-model="password"
-          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-          :type="showPassword ? 'text' : 'password'"
+          v-model="passwordConfirm"
+          :append-icon="showPasswordConfirm ? 'mdi-eye' : 'mdi-eye-off'"
+          :type="showPasswordConfirm ? 'text' : 'password'"
           name="input-10-1"
           prepend-icon="mdi-key"
           label="パスワード(確認)"
           counter
-          @click:append="showPassword = !showPassword"
+          @click:append="showPasswordConfirm = !showPasswordConfirm"
           required
         ></v-text-field>
 
@@ -74,7 +74,7 @@
     </v-card-text>
     <v-card-actions>
       <v-spacer></v-spacer>
-      <v-btn color="yellow darken-1" @click="userSignUp">登録</v-btn>
+      <v-btn color="yellow darken-1">登録</v-btn>
     </v-card-actions>
   </v-card>
 </template>
@@ -84,24 +84,18 @@ import "@mdi/font/css/materialdesignicons.css";
 export default {
   name: "SignUp",
   data: () => ({
-    username: "",
+    email: "",
+    emailConfirm: "",
+    nickname: "",
+    lastName: "",
+    firstName: "",
+    lastNameKana: "",
+    firstNameKana: "",
     password: "",
-    showPassword: false
-  }),
-  methods: {
-    userSignUp() {
-      this.$store
-        .dispatch("userSignUp", {
-          username: this.username,
-          password: this.password
-        })
-        .then(() => {
-          this.$router.push({ path: "/#/home" });
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    }
-  }
+    passwordConfirm: "",
+    checkbox: false,
+    showPassword: false,
+    showPasswordConfirm: false
+  })
 };
 </script>
