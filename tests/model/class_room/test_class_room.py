2ff6ffd3cae92be3fffb7db0462e32cf00d293aa
Naoki Miyoshi <trombone344@gmail.com>
2020-02-28 01:13:44 +0900

    fix: テストの修正

--- tests/model/class_room/test_class_room.py
+++ tests/model/class_room/test_class_room.py
@@ -14,3 +14,16 @@ class ClassRoomTest(TestCase):
             'capacity': 10
         }
         self.assertEqual(ClassRoom.from_dict(class_dict).to_dict(), class_dict)
+
+    def test_dict_min(self):
+        class_dict = {
+            'class_room_id': 1,
+            'name': 'test name',
+            'image_url': 'https://example.example.com/test.jpg',
+            'publish_type': 'private',
+            'tag_list': ['tag1', 'tag2']
+        }
+        self.assertEqual(
+            ClassRoom.from_dict(class_dict).to_dict(), {
+                **class_dict, 'capacity': None
+            })
