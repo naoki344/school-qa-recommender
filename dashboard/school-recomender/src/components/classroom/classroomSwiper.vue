f720e5fe750885335297358a3ced28429a4894a1
濱砂大輝 <vetmal.0377.d@gmail.com>
2020-03-07 14:18:49 +0900

    fix: データ構造を変更

--- dashboard/school-recomender/src/components/classroom/classroomSwiper.vue
+++ dashboard/school-recomender/src/components/classroom/classroomSwiper.vue
@@ -1,53 +1,64 @@
 <!-- The ref attr used to find the swiper instance -->
 <template>
   <v-container>
-    <swiper class="classroom-swiper-thumbs" :options="swiperOptionThumbs" ref="swiperThumbs">
-      <swiper-slide v-for="myClass in classroomList" :key="myClass.classroom.classroom_id" >{{myClass.classroom.name}}</swiper-slide>
+    <swiper
+      class="classroom-swiper-thumbs"
+      :options="swiperOptionThumbs"
+      ref="swiperThumbs"
+    >
+      <swiper-slide
+        v-for="myClass in classroomList"
+        :key="myClass.classroom.classroom_id"
+        >{{ myClass.classroom.name }}</swiper-slide
+      >
     </swiper>
     <swiper :options="swiperOptionTop" ref="swiperTop">
-      <swiper-slide v-for="myClass in classroomList" :key="myClass.classroom.classroom_id" >
-        <h1>{{myClass.classroom.name}}</h1>
-        <h1>{{myClass.classmate.join_status}}</h1>
+      <swiper-slide
+        v-for="myClass in classroomList"
+        :key="myClass.classroom.classroom_id"
+      >
+        <h1>{{ myClass.classroom.name }}</h1>
+        <h1>{{ myClass.classmate.join_status }}</h1>
       </swiper-slide>
       <!-- Optional controls -->
-      <div class="swiper-pagination"  slot="pagination"></div>
+      <div class="swiper-pagination" slot="pagination"></div>
       <div class="swiper-button-prev" slot="button-prev"></div>
       <div class="swiper-button-next" slot="button-next"></div>
-      <div class="swiper-scrollbar"   slot="scrollbar"></div>
+      <div class="swiper-scrollbar" slot="scrollbar"></div>
     </swiper>
   </v-container>
 </template>
 
 <script>
 import { mapState } from "vuex";
-import 'swiper/dist/css/swiper.css'
-import { swiper, swiperSlide } from 'vue-awesome-swiper'
+import "swiper/dist/css/swiper.css";
+import { swiper, swiperSlide } from "vue-awesome-swiper";
 
 export default {
   name: "classroomSwiper",
   components: {
     swiper,
-    swiperSlide,
+    swiperSlide
   },
   data() {
     return {
       swiperOptionTop: {
         spaceBetween: 10,
         navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev'
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev"
         }
       },
       swiperOptionThumbs: {
         spaceBetween: 20,
         centeredSlides: true,
-        slidesPerView: 'auto',
+        slidesPerView: "auto",
         touchRatio: 0.2,
         slideToClickedSlide: true,
-		watchSlidesVisibility: true,
-        watchSlidesProgress: true,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true
       }
-    }
+    };
   },
   computed: {
     ...mapState({
@@ -57,22 +68,21 @@ export default {
   methods: {
     fetchClassroomList() {
       this.$store.dispatch("classroom/fetchMyClassroomList");
-    },
+    }
   },
   mounted() {
     this.$nextTick(() => {
-      const swiperTop = this.$refs.swiperTop.swiper
-      const swiperThumbs = this.$refs.swiperThumbs.swiper
-      swiperTop.controller.control = swiperThumbs
-      swiperThumbs.controller.control = swiperTop
+      const swiperTop = this.$refs.swiperTop.swiper;
+      const swiperThumbs = this.$refs.swiperThumbs.swiper;
+      swiperTop.controller.control = swiperThumbs;
+      swiperThumbs.controller.control = swiperTop;
     });
     this.fetchClassroomList();
   }
-}
+};
 </script>
 
-
-<style lang="scss" scoped >
+<style lang="scss" scoped>
 .classroom-swiper-thumbs {
   .swiper-slide {
     width: auto;
